6caf284461d53909922d640bfe8928225cd0950a
Aeisha Hicks <ashicks2017@outlook.com>
2019-07-16 18:54:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+// Your code goes hedocument.addEventListener( "DOMContentLoaded", function () {
+   
+
+      document.addEventListener("DOMContentLoaded", function() {
+          updateDOM()
+      } );
+
+      function updateDOM() {
+           document.getElementById ( "text" )
+           .innerHTML = "This is really cool!"; 
+      }
